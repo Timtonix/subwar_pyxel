f534efda2605f009c4150e6b30dc6e20b9aac346
timtonix <timtonic@outlook.fr>
2023-01-12 19:17:04 +0100

    I create the grid line 8

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pyxel
 import random
 
+
 class SubWar():
     def __init__(self):
         pyxel.init(128, 128, title="BattleShip")
-
+        self.grid = [[' ']*10 for x in range(10)]
 
     def update(self):
         pass
