b6d7775a4efc45b8a42850316ab7126df1cb139a
timtonix <timtonic@outlook.fr>
2023-01-14 11:15:58 +0100

    update #2

--- main.py
+++ main.py
@@ -16,31 +16,36 @@ class SubWar:
         self.hidden_grid = [[' '] for _ in range(10)]  # Create a nested list of 10*10
         self.grid_x = 0
         self.grid_y = 0
+        self.grid_i =  0
 
-        #
+        pyxel.mouse(True)
 
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
-
+        if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT):
+            pyxel.text(0, 100, "Mouse CLICK", col=3)
+            print("clicked")
 
     def update_submarines(self):
         pass
 
-
     def draw(self):
         pyxel.cls(0)
-
         # Create the grid
-        for _ in self.hidden_grid:
+        while self.grid_i < 10:
             for _ in self.hidden_grid:
-                pyxel.rect(self.grid_x, self.grid_y, 6, 6, 1)
-                self.grid_x += 10
-            self.grid_y += 10
-            self.grid_x = 0
+                for _ in self.hidden_grid:
+                    pyxel.rect(self.grid_x, self.grid_y, 6, 6, 1)
+                    self.grid_x += 10
+                self.grid_y += 10
+                self.grid_x = 0
+                self.grid_i += 1
+                print(f"On a fini la {self.grid_i} grille")
+        self.grid_x = 0
+        self.grid_y = 0
+        self.grid_i = 0
 
-        pyxel.show()
 
 
 SubWar()
