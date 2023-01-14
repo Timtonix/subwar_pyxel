de162ebfd87cb6452f11c153c5ca0f64ddc7b212
timtonix <timtonic@outlook.fr>
2023-01-14 10:58:13 +0100

    I create the grid line 8

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 import pyxel
 
+"""
+List of submarines :
+
+- Ukrainian Tiger : 3 points
+- French Nuclear : 4 points
+- US Spider*2 : 2 points
+- Taiwanese Dragon : 3 points
+"""
+
 
 class SubWar:
     def __init__(self):
         pyxel.init(160, 120, title="BattleShip")
         self.hidden_grid = [[' '] for _ in range(10)]  # Create a nested list of 10*10
-        self.x = 0
-        self.y = 0
+        self.grid_x = 0
+        self.grid_y = 0
+
+        #
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
         pass
 
+
+    def update_submarines(self):
+        pass
+
+
     def draw(self):
         pyxel.cls(0)
+
+        # Create the grid
         for _ in self.hidden_grid:
             for _ in self.hidden_grid:
-                pyxel.rect(self.x, self.y, 2, 2, 1)
-                self.x += 4
-            self.y += 4
-            self.x = 0
+                pyxel.rect(self.grid_x, self.grid_y, 6, 6, 1)
+                self.grid_x += 10
+            self.grid_y += 10
+            self.grid_x = 0
 
         pyxel.show()
 
