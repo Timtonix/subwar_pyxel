2d4c70138a00a2df0b078375d18d4b74052c70e4
timtonix <timtonic@outlook.fr>
2023-01-20 22:06:26 +0100

    Fix #4 v0.1 it just places one-square ship

---
+++ boat.py
@@ -0,0 +1,20 @@
+from random import randint
+
+
+class Boat:
+    def __init__(self, ships: int):
+        self.bot_grid = [['' for _ in range(10)] for _ in range(10)]
+        self.ships = ships
+
+    def place_ships(self) -> list: # Now it just place ships of 1 case
+        for ship in range(self.ships):
+            ship_row, ship_line = randint(0, 9), randint(0, 9)
+            while self.bot_grid[ship_row][ship_line] == 'X':
+                ship_row, ship_line = randint(0, 9), randint(0, 9)
+            self.bot_grid[ship_row][ship_line] = 'X'
+        print(self.bot_grid)
+
+
+boat = Boat(5)
+boat.place_ships()
+
