f1d0edb8c5a22e937b49bb4b9a14e322bb529143
timtonix <timtonic@outlook.fr>
2023-01-21 09:15:33 +0100

    Fix #19 release the v1 resolve #6

--- boat.py
+++ boat.py
@@ -9,13 +9,22 @@ class Boat:
     def place_ships(self) -> list: # Now it just place ships of 1 case
         for ship in range(self.ships):
             ship_row, ship_line = randint(0, 9), randint(0, 9)
-            while self.bot_grid[ship_row][ship_line] == 'X':
+            while self.bot_grid[ship_row][ship_line] == 'Boat':
                 ship_row, ship_line = randint(0, 9), randint(0, 9)
-            self.bot_grid[ship_row][ship_line] = 'X'
+            self.bot_grid[ship_row][ship_line] = 'Boat'
         print(self.bot_grid)
 
     def is_there_a_boat(self, coordinates: tuple) -> bool:
         # Coordinates accept value between 0 and 9 for the row and line
-        if self.bot_grid[coordinates[0]][coordinates[1]]:
+        if self.bot_grid[coordinates[0]][coordinates[1]] == 'Destroyed':
             return True
+        elif self.bot_grid[coordinates[0]][coordinates[1]] == 'Boat':
+            self.bot_grid[coordinates[0]][coordinates[1]] = 'Destroyed'
+            print(self.bot_grid)
+            self.ships -= 1
+            return True
+
         return False
+
+    def remaining_boat(self):
+        return self.ships
