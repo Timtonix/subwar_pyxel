eb8e5163744dd28a6fad72f0e8e515e9366df30b
timtonix <timtonic@outlook.fr>
2023-01-21 08:34:56 +0100

    Refactor the code

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ class SubWar:
     def __init__(self):
         pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="BattleShip")
         self.color_grid = [[7 for _ in range(10)] for _ in range(10)]  # Create a nested list of 10*10
-        self.gps_grid = [[[''] for _ in range(10)] for _ in range(10)]
+        self.gps_grid = [[0 for _ in range(10)] for _ in range(10)]
         self.circ_radius = 4
         self.space_between = 11
         self.default_grid_x = (SCREEN_WIDTH - (9 * self.space_between + self.circ_radius)) / 2
         self.default_grid_y = (SCREEN_HEIGHT - (9 * self.space_between + self.circ_radius)) / 2
-        self.grid_i = 0
         self.mouse_pos = (pyxel.mouse_x, pyxel.mouse_y)
         pyxel.mouse(True)
+
 
         pyxel.run(self.update, self.draw)
 
@@ -56,23 +56,20 @@ class SubWar:
                             area = 36
                             
                         """
+                        print(self.gps_grid)
                         print(f"Clicked on {self.gps_grid[row][line][0]} - {self.gps_grid[row][line][1]}")
 
     def draw_grid(self):
         x = self.default_grid_x
         y = self.default_grid_y
-        self.grid_i = 0
-
-        while self.grid_i < 10:
-            grid_length = len(self.ship_grid)
-            for row in range(grid_length):
-                for line in range(grid_length):
-                    pyxel.circ(x, y, self.circ_radius, self.color_grid[row][line])
-                    self.gps_grid[row][line] = (x, y)
-                    x += self.space_between
-                y += self.space_between
-                x = self.default_grid_x
-                self.grid_i += 1
+        grid_length = len(self.color_grid)
+        for row in range(grid_length):
+            for line in range(grid_length):
+                pyxel.circ(x, y, self.circ_radius, self.color_grid[row][line])
+                self.gps_grid[row][line] = (x, y)
+                x += self.space_between
+            y += self.space_between
+            x = self.default_grid_x
 
     def draw(self):
         pyxel.cls(0)
